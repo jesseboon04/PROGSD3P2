3ab8a6ea71520f0b1aa4c61bcaf55fde8d3e6949
jesseboon04 <30845@ma-web.nl>
2022-12-21 14:27:04 +0100

    week 1 opdracht 4 basis

--- PROGBasis-week1/arrays.php
+++ PROGBasis-week1/arrays.php
@@ -2,7 +2,7 @@
 
 $aanhef = array(
     "aanhef" => "Beste",
-    "persoon" => "");
+    "persoon" => "klant");
 
 $korting = array(
     "product" => "U heeft in de voorgaande bestelling voor [[product]] gekozen",
@@ -11,15 +11,21 @@ $korting = array(
 
 $ondertekening = array(
   "groet" => "Met vriendelijke groet,",
-  "naam" => "",
+  "naam" => "Jesse Boon",
   "functie" => "Directeur Webshop beheer"
 );
 
+
+echo '<pre>';
+var_dump($aanhef);
+var_dump($korting);
+
+var_dump($ondertekening);
+echo  '</pre>';
 /*
  * Opdracht 1:
  * Laat de inhoud van alle arrays zien
  * Gebruik daarvoor de functies var_dump() en print_r()
- */
 
 /*
  * Opdracht 2:
@@ -32,13 +38,19 @@ $ondertekening = array(
  * Vervang het woord [[product]] door jouw gekozen product in de winkelmand opdracht
  * Gebruik hiervoor de methode str_replace().
  */
+ echo str_replace($korting,"[[product]]","pasta");
 
 /*
  * Opdracht 4:
  * Voeg nu alle arrays samen in een nieuwe array die je $aanbieding noemt.
  * Gebruik hiervoor de methode array_merge()
  */
+echo "<br>";
+echo "<br>";
+echo "<br>";
 
+$aanbieding = array($aanhef,"<br>",$korting,"<br>",$ondertekening);
+print_r($aanbieding);
 /*
  * Opdracht 5:
  * Toon de inhoud van de nieuwe array $aanbieding
